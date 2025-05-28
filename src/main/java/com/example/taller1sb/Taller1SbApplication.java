5d87fa8f376fab4e2571676c74ee13ca1231035a
Sofialm129 <solozanom@udistrital.edu.co>
2025-05-28 16:23:02 -0500

    Initial commit

---
+++ src/main/java/com/example/taller1sb/Taller1SbApplication.java
@@ -0,0 +1,13 @@
+package com.example.taller1sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller1SbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Taller1SbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/taller1sb/Taller1SbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taller1sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller1SbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
